c064b2b349f864dd7d90ca3517544f7e279463a3
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-02-06 11:56:40 +0530

    Adding Comments

--- core/tests.py
+++ core/tests.py
@@ -8,16 +8,17 @@ from .serializers import PersonSerializer
 
 from rest_framework import status
 
-PERSON_URL = reverse('core:person-list')
+PERSON_URL = reverse('core:person-list') ## API URL
+
 
 
 class PrivatePersonApiTests(TestCase):
     """Test the authorized user tags API"""
 
     def setUp(self):
-        self.client = APIClient()
+        self.client = APIClient() ## API CLIENT by Django TO TEST
 
-    def test_retrieve_person(self):
+    def test_retrieve_person(self):  ## Method for testing retrive (GET Operation)
         """Test retrieving tags"""
         Person.objects.create(
             checked=True,
@@ -26,25 +27,25 @@ class PrivatePersonApiTests(TestCase):
             age=23,
             description='Test Disc',
             date=datetime.datetime.now()
-            )
+            )  ## Creating Test PERSON
 
-        res = self.client.get(PERSON_URL)
+        res = self.client.get(PERSON_URL) ## Taking Data through API URL
 
-        tags = Person.objects.all().order_by('-name')
-        serializer = PersonSerializer(tags, many=True)
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data, serializer.data)
+        persons = Person.objects.all().order_by('-name')  ## Collecting Data from Person Model to Validate with res(Data Retrived through API Client) 
+        serializer = PersonSerializer(persons, many=True) ## Serielize The data 
+        self.assertEqual(res.status_code, status.HTTP_200_OK)  ## Check API is Worked 
+        self.assertEqual(res.data, serializer.data) ## Check Datas are same 
 
    
 
-    def test_create_person_successful(self):
-        payload = {'checked':True,'name': 'Simple','type':'Type1','age':23,'description':'description1','date':datetime.datetime.now()}
-        self.client.post(PERSON_URL, payload)
+    def test_create_person_successful(self): # Test For Create Person
+        payload = {'checked':True,'name': 'Simple','type':'Type1','age':23,'description':'description1','date':datetime.datetime.now()} ## Payload Data
+        self.client.post(PERSON_URL, payload) ## Call Post MEthod
 
         exists = Person.objects.filter(
             name=payload['name']
         ).exists()
-        self.assertTrue(exists)
+        self.assertTrue(exists)  ## Check data is Posted And Exist 
 
   
 
