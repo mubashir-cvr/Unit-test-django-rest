f02d4ead587bde7a21b4001ef7c3cbe5aa0ebc78
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-02-06 10:53:24 +0530

    Adding Model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    checked=models.BooleanField()
+    name=models.CharField(max_length=225)
+    type=models.CharField(max_length=225)
+    age=models.IntegerField(max_length=225)
+    description=models.CharField(max_length=225)
+    description=models.CharField(max_length=225)
+
